97f3d2266e470fe7b12e0720714070dd26be3018
OharaYusuk <u193944j@ecs.osaka-u.ac.jp>
2021-10-30 01:04:48 +0900

    ohara change3

--- findmycospa-app/src/App.js
+++ findmycospa-app/src/App.js
@@ -8,13 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-<<<<<<< HEAD
-          Do my best! Ozu is the best!!
-          Yeah!!
-=======
           Do my best! OhaYu is the best!!!!!!!!!!!!!!!!!!!!!!!
           Yeah!! Ohara or Not?
->>>>>>> 95269cb1df5604fd9664079cd15e62b056855f0f
         </p>
         <a
           className="App-link"
