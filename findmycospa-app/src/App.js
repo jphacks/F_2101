95269cb1df5604fd9664079cd15e62b056855f0f
YukiFujiya <93020304+YukiFujiya@users.noreply.github.com>
2021-10-30 00:37:23 +0900

    Update App.js

--- findmycospa-app/src/App.js
+++ findmycospa-app/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
           Do my best! OhaYu is the best!!!!!!!!!!!!!!!!!!!!!!!
-          Yeah!!
+          Yeah!! Ohara or Not?
         </p>
         <a
           className="App-link"
